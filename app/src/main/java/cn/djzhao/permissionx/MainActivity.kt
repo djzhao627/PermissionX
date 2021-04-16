9837695e3f9e94e539db6caf46e731671db7094c
djzhao <djzhao627@gmail.com>
2021-04-16 19:40:09 +0800

    完成 PermissionX的开发与测试

--- app/src/main/java/cn/djzhao/permissionx/MainActivity.kt
+++ app/src/main/java/cn/djzhao/permissionx/MainActivity.kt
@@ -1,11 +1,36 @@
 package cn.djzhao.permissionx
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        makeCallBtn.setOnClickListener {
+            PermissionX.request(this, Manifest.permission.CALL_PHONE) { allGranted, deniedList ->
+                if (allGranted) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You Denied $deniedList", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+    fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e:Exception) {
+            e.printStackTrace()
+        }
     }
 }
